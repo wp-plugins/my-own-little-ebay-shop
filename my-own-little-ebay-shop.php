e59c762e5db3b9c7ec259f16ea7b944c4133613d
realTM <realTM@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-17 15:56:06 +0000

    updated version number
    git-svn-id: https://plugins.svn.wordpress.org/my-own-little-ebay-shop/trunk@242371 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- my-own-little-ebay-shop.php
+++ my-own-little-ebay-shop.php
@@ -3,7 +3,7 @@
 Plugin Name: My own little eBay Shop
 Plugin URI: http://fuck-dance-lets-art.com/my-own-little-ebay-shop-wordpress-plugin
 Description: **THIS IS A ALPHA VERSION, CHANGES WILL HAPPEN!, THE FIRST RELEASE WILL COME VERY SOON**. Very fast plugin that cache your shop's content for a quicker and smoother user experience. Easy to set up, with some clever functionalities,  including retrieving your shop categories, excluding categories, renaming categories (only in wordpress), set a "refresh temp file" time. Displays your ebay shop listing, items infos (pictures, bids, price, etc‚ ...) with links to your listing and shop.
-Version: 0.3
+Version: 0.2.1
 Author: Thomas Michalak aka TM
 Author URI: http://fuck-dance-lets-art.com/
 */
